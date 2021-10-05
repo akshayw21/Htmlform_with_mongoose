bdf5fc7a8c718b85022aca0841fad47a8f146a19
akshayw21 <akshaywohlig21@gmail.com>
2021-10-05 10:41:02 +0530

    update1

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.get('/',(req,res,next)=>{
 });
 
 app.get('/contact',(req,res)=>{
-     User.find({}).then(result=>{
+     User.find({}).sort({_id:-1}).then(result=>{
          res.status(200).json({
              userSchema:result
          });
